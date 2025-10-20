61f834b26dcd90dc5a24e3fe95507a711b693223
sozercanye <234424372+sozercanye@users.noreply.github.com>
2025-10-20 21:12:19 +0300

    Importing few functions instead of library.

--- teleporter/desktop/utils/create_local_key.py
+++ teleporter/desktop/utils/create_local_key.py
@@ -1,12 +1,12 @@
-import hashlib
+from hashlib import sha512, pbkdf2_hmac
 
 def create_local_key(
     salt: bytes,
     passcode: bytes = b''
 ) -> bytes:
-    hash_key = hashlib.sha512(salt)
+    hash_key = sha512(salt)
     hash_key.update(passcode)
     hash_key.update(salt)
 
     iterations = 1 if not passcode else 100_000
-    return hashlib.pbkdf2_hmac('sha512', hash_key.digest(), salt, iterations, 256)
+    return pbkdf2_hmac('sha512', hash_key.digest(), salt, iterations, 256)
