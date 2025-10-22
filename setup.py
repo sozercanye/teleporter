1faf946f83258b65646adebc1e3ff894780a2671
sozercanye <234424372+sozercanye@users.noreply.github.com>
2025-10-22 13:38:37 +0300

    Fixed building.

--- setup.py
+++ setup.py
@@ -4,23 +4,16 @@ import setuptools
 from teleporter import __version__
 
 directory = Path(__file__).parent
-
-with open(directory / 'README.md') as f:
-    long_description = '\n' + f.read()
-
-with open(directory / 'requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
 setuptools.setup(
     name='teleporter',
     version=__version__,
     author='sozercanye',
     description='Serializer and deserializer for Telegram Android and Desktop sessions.',
     long_description_content_type='text/markdown',
-    long_description=long_description,
+    long_description='\n' + open(directory / 'README.md').read(),
     url='https://github.com/sozercanye/teleporter',
     packages=setuptools.find_packages(),
     license='MIT',
-    install_requires=install_requires,
+    install_requires=open(directory / 'requirements.txt').read().splitlines(),
     python_requires='>=3.9'
 )
